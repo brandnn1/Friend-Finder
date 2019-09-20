a18844bd56410fa1b67baa616555bccda3eebb60
brandnn <brandnn@uw.edu>
2019-09-19 18:22:23 -0700

    coded matching logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -35,14 +35,38 @@ app.post('/api/friends', function(req,res){
     //grabs the new friend's scores to compare with friends in friendList array
     console.log(req.body.scores);  // <-- Does not exist
 
-
-    var newFriend = req.body;
-
-
-    //pushes new submission into the friendsList array
-    friendList.push(newFriend);
-    res.json(newFriend)
-});
+      //grabs the new friend's scores to compare with friends in friendList array
+      var newFriend = req.body;
+      var newFriendScores = req.body.scores;
+      var scoresArray = [];
+      var bestMatch = 0;
+  
+      //runs through all current friends in list
+      for(var i=0; i<friendList.length; i++){
+        var scoresDiff = 0;
+        //run through scores to compare friends
+        for(var j=0; j<newFriendScores.length; j++){
+          scoresDiff += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newFriendScores[j])));
+        }
+  
+        //push results into scoresArray
+        scoresArray.push(scoresDiff);
+      }
+  
+      //after all friends are compared, find best match
+      for(var i=0; i<scoresArray.length; i++){
+        if(scoresArray[i] <= scoresArray[bestMatch]){
+          bestMatch = i;
+        }
+      }
+  
+      //return bestMatch data
+      var bff = friendList[bestMatch];
+      res.json(bff);
+  
+      //pushes new submission into the friendsList array
+      friendList.push(newFriend);
+  });
 
 //////////////////////////
 
@@ -55,32 +79,3 @@ app.get("/friendsList", function(req, res) {
     });
   });
   
-
-  app.post("/friendsList", function(req, res) {
-    
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    console.log('SERVER SIDE: ', req.body)
-    var newReservation = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newReservation
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    //newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
-  
-    // console.log(newReservation);
-  
-    connection.query("INSERT INTO friendsList SET ?",
-    {
-      name: newReservation.name,
-    role: newReservation.role,
-    date: newReservation.date,
-    time: newReservation.time
-    },
-    function(err, result) {
-      if (err) throw err;
-      res.json(newReservation);
-
-
-  })
-})
-};
--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 var express = require("express");
 //path is native to node library. Allows usage  of file paths to get current relative directory
 var path = require("path");
-var bodyParser = require('body-parser')
+
 // Sets up the Express App
 // =============================================================
 //creating instance of express
@@ -20,10 +20,6 @@ var PORT = process.env.PORT || 8080;
 //If these are not included, in POST route there will not be a req.body.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
 // all routes - that's where we are getting our data from 
